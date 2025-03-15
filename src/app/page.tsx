ea053d7e533ff97cf739c08ea7f8abbdacd115d5
John <john6tain.tube@gmail.com>
2025-03-15 11:17:19 +0200

    Added redirect after after loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 'use client';
 import Image from "next/image";
+import {useRouter} from "next/navigation";
 
 export default function Home() {
+
+	const router = useRouter();
+	setTimeout(() => {
+		router.push('/dashboard');
+	}, 1000)
+
 	return (
 		<div
 			className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
